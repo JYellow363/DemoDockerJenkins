e9134b3c4dfc307077581175fb40ad139aca3728
JYellow363 <jyellow363@gmail.com>
2020-06-24 01:43:18 -0500

    first commit

---
+++ src/main/java/pe/edu/upc/boundary/GreetingResource.java
@@ -0,0 +1,12 @@
+package pe.edu.upc.boundary;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("greeting")
+public class GreetingResource {
+    @GET
+    public String greet(){
+        return "Hello UPC";
+    }
+}
